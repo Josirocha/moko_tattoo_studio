eb15f9fb5fc4e97c6cd826fedbaf7b709d7d912c
alonso-estevam <alonso_estevam@outlook.com>
2022-07-27 23:31:36 -0300

    feat: exportando tatuadoresController

--- src/controller/tatuadores-controller.js
+++ src/controller/tatuadores-controller.js
@@ -1,5 +1,7 @@
-const tatuadores = (app) =>{
+const tatuadoresController = (app) =>{
     app.get('/tatuadores', (req, res) => {
     res.send('Rota GET para tatuadores')
   })
 }
+
+export default tatuadoresController
