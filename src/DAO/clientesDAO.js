b6b20316b2a65f119654341622ef7b64999ec49d
Rômulo Miranda <romulo.mgaudencio@gmail.com>
2022-07-31 18:06:09 -0300

    implementado o crud completo da DAO

--- src/DAO/clientesDAO.js
+++ src/DAO/clientesDAO.js
@@ -2,12 +2,26 @@ import db from "../database/database.js"
 
 const dao = {
 
-    verClientes : () => {
-        const PEGA_CLIENTES = `
-        SELECT * FROM CLIENTES
-        `
-        return new Promise((resolve, reject)=>{
-            db.all(PEGA_CLIENTES, (error,row)=>{
+    listarCliente : (id) => {
+        const query = "SELECT * FROM CLIENTES WHERE id = ?"
+        return new Promise((resolve, reject) => {
+            db.get(query, id, (error,row) => {
+                if(error) {
+                    reject(error)
+                } else if ((!row) || row.length <= 0) {
+                    reject({
+                        "message": 'Usuário não encontrado',
+                        "erro": true
+                    })
+                } else {resolve(row)}
+            })
+        })
+    },
+
+    listarClientes : () => {
+        const query = `SELECT * FROM CLIENTES`
+        return new Promise((resolve, reject) => {
+            db.all(query, (error,row) => {
                 if(error)
                     reject(error)
                 else
@@ -17,18 +31,47 @@ const dao = {
     },
 
     criarCliente : (usuario) => {
-        const CRIA_CLIENTE = `INSERT INTO CLIENTES (NOME, TELEFONE)
+        const query = `INSERT INTO CLIENTES (NOME, TELEFONE)
         VALUES (?, ?)`
 
         return new Promise((resolve, reject) => {
-            db.run(CRIA_CLIENTE, usuario.nome, usuario.telefone, (error,row) => {
+            db.run(query, ...Object.values(usuario), (error,row) => {
                 if(error)
                     reject(error)
                 else
                     resolve(row)
             })
         })
+    },
+
+    atualizarCliente : (id, novo) => {
+        const query = `UPDATE CLIENTES SET nome = ?, telefone = ? WHERE id = ?`
+
+        return new Promise((resolve, reject)=>{
+            db.run(query, ...Object.values(novo), id, (error) => {
+                    if(error)
+                        reject(error)
+                    else
+                        resolve(novo)
+                }
+            )
+        })  
+    },
+
+    deletarCliente : (id) => {
+        const query = `DELETE FROM CLIENTES WHERE id = ?`
+
+        return new Promise((resolve, reject) => {
+            db.run(query, id, (error, row) => {
+                if(error)
+                        reject(error)
+                    else
+                        resolve(row)
+                })
+        })
     }
+
 }
+
 
 export default dao
