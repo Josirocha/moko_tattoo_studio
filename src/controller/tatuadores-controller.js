03f7fe643a69a96ac0bc713268755cc9f2e507e3
alonso-estevam <alonso_estevam@outlook.com>
2022-07-31 20:02:46 -0300

    feat: importando tatuadoresDAO

--- src/controller/tatuadores-controller.js
+++ src/controller/tatuadores-controller.js
@@ -1,7 +1,25 @@
-const tatuadoresController = (app) =>{
-    app.get('/tatuadores', (req, res) => {
-    res.send('Rota GET para tatuadores')
-  })
-}
+import tatuadoresDAO from "../DAO/tatuadoresDAO";
+import tatuadoresModel from "../model/tatuadores-model";
 
+const tatuadoresController = {
+  listarTatuadores: async (req, res) => {
+    try {
+      const resposta = await tatuadoresDAO.listarTatuadores();
+      res.status(200).json(resposta);
+    } catch (e) {
+      res.status(400).json(e.message);
+    }
+  },
+
+  listarTatuador: async (req, res) => {
+    const id = req.params.id
+    try {
+      const resposta = await tatuadoresDAO.listarTatuador(id);
+      res.status(200).json(resposta);
+    } catch (e) {
+      res.status(404).json(e.message);
+    }
+  }
+
+}
 export default tatuadoresController
