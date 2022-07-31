6a57e2fae1645ae48ccc9d321c6d62da2ffc8467
Josirocha <josi1992@gmail.com>
2022-07-31 15:38:25 -0300

    feat: importando a agendamentos Routes para o index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,17 @@
 import express from 'express';
-import clientes from './clientesRoutes.js'
+// import clientes from './clientesRoutes.js'
+import agendamentos from './agendamentosRoutes.js'
 
 const routes = (app) => {
-    app.route('/').get((req, res) => {
-      res.status(200).send({titulo: "Moko Tattoo Studio"})
-    })
-  
-    app.use(
-      express.json(),
-      clientes
-    )
-  }
-  
-  export default routes
+  app.route('/').get((req, res) => {
+    res.status(200).send({ titulo: "Moko Tattoo Studio" })
+  })
+
+  app.use(
+    express.json(),
+    // clientes
+    agendamentos
+  )
+}
+
+export default routes
