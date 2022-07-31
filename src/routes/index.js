994823fbc5dc344567438ddf93584564fefaa093
BeatrizMioranza <beatrizmioranza2508@gmail.com>
2022-07-31 18:06:46 -0300

    ajustei na rota get de tatuagensRoutes.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import clientes from './clientesRoutes.js'
 import agendamentos from './agendamentosRoutes.js'
+import tatuagens from './tatuagensRoutes.js'
 
 const routes = (app) => {
   app.route('/').get((req, res) => {
@@ -10,8 +11,8 @@ const routes = (app) => {
   app.use(
     express.json(),
     clientes,
-    agendamentos
+    agendamentos, 
+    tatuagens
   )
 }
 
-export default routes
--- src/routes/tatuagensRoutes.js
+++ src/routes/tatuagensRoutes.js
@@ -6,6 +6,9 @@ const router = express.Router();
 router
     .get("/tatuagens", tatuagensController.imagensTatto)
     .get("tatuagens/:categoria", )
+    .post("tatuagens/")
+    .put("tatuagens/")
+    .delete("tatuagens/")
 
 
 export default router;
