26e2c862dbef31054d8bc1459d586bb55a1276f5
alonso-estevam <alonso_estevam@outlook.com>
2022-07-31 19:11:32 -0300

    feat: criando arquivo tatuadoresDAO.js

---
+++ src/DAO/tatuadoresDAO.js
@@ -0,0 +1,21 @@
+import db from "../database/database.js"
+
+const tatuadoresDAO = {
+    listarCliente : (id) => {
+        const query = "SELECT * FROM TATUADORES WHERE id = ?"
+        return new Promise((resolve, reject) => {
+            db.get(query, id, (error,row) => {
+                if(error) {
+                    reject(error)
+                } else if ((!row) || row.length <= 0) {
+                    reject({
+                        "message": 'Tatuador não encontrado',
+                        "erro": true
+                    })
+                } else {resolve(row)}
+            })
+        })
+    }
+}
+
+export default tatuadoresDAO
