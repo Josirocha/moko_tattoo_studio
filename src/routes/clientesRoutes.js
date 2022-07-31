a5436e45c122e058761d0b19bfd1b7dc2ee39229
Rômulo Miranda <romulo.mgaudencio@gmail.com>
2022-07-31 18:06:29 -0300

    todas as rotas do crud implementadas no router

--- src/routes/clientesRoutes.js
+++ src/routes/clientesRoutes.js
@@ -5,8 +5,10 @@ const router = express.Router();
 
 router
     .get("/clientes", clienteController.listarClientes)
-    // .get("/clientes/:id", clienteController.listarClientesId)
+    .get("/clientes/:id", clienteController.listarCliente)
     .post("/clientes", clienteController.criarCliente)
+    .put("/clientes/:id", clienteController.atualizarCliente)
+    .delete("/clientes/:id", clienteController.deletarCliente)
 
 
 export default router;
