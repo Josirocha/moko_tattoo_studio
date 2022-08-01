d343352908f4ac6d83d8d4ba81c353f4eaeb4351
alonso-estevam <alonso_estevam@outlook.com>
2022-08-01 14:11:46 -0300

    fix: erro na rota tatuadores

--- src/routes/tatuadoresRoutes.js
+++ src/routes/tatuadoresRoutes.js
@@ -4,4 +4,4 @@ import tatuadoresController from "../controller/tatuadores-controller";
 const router = express.Router();
 
 router
-    .get('/', tatuadoresController.listarTatuadores)
+    .get('/tatuadores', tatuadoresController.listarTatuadores)
