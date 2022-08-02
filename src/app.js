955ad2f67192c1be5422d6be5fb03ac6c2591ade
Rômulo Miranda <romulo.mgaudencio@gmail.com>
2022-08-02 12:23:20 -0300

    implementado cors no app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
   import express from 'express'
   import routes from './routes/index.js'
+  import cors from 'cors'
 
   const app = express()
   
+  app.use(cors())
   app.use(express.json())
   
   routes(app)
