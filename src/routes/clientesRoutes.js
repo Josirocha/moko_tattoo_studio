54d9d9f962c5cfb7fe647d3a7f5e5e9ba641f109
Rômulo Miranda <romulo.mgaudencio@gmail.com>
2022-08-02 17:32:57 -0300

    nome dos métodos semânticos

--- src/routes/clientesRoutes.js
+++ src/routes/clientesRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router
     .get("/clientes", clienteController.listarClientes)
     .get("/clientes/:id", clienteController.listarCliente)
-    .post("/clientes", clienteController.criarCliente)
+    .post("/clientes", clienteController.cadastrarCliente)
     .put("/clientes/:id", clienteController.atualizarCliente)
     .delete("/clientes/:id", clienteController.deletarCliente)
 
