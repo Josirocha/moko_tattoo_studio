c08e5c516eddec8a108ca1e62ec388258d74cf8c
BeatrizMioranza <beatrizmioranza2508@gmail.com>
2022-08-02 19:52:05 -0300

    ajuste na indentação de tatuagensDAO.js

--- src/DAO/tatuagensDAO.js
+++ src/DAO/tatuagensDAO.js
@@ -3,63 +3,69 @@ import db from "../database/database.js"
 const dao = {
 
     verTatuagens: () => {
-        const pegaTattoo = ` SELECT * FROM TATUAGENS `
-        return new Promise((resolve, reject)=>{
-            db.all(pegaTattoo, (error,row)=>{
-                if(error)
+        const pegaTattoo = "SELECT * FROM TATUAGENS"
+        return new Promise((resolve, reject) => {
+            db.all(pegaTattoo, (error, row) => {
+                if (error) {
                     reject(error)
-                else
+                } else {
                     resolve(row)
+                }
             })
         })
     },
 
     verTatuagem: (id) => {
-        const pegaTattoo = "SELECT * FROM TATUAGENS WHERE id = ?"
-        return new Promise((resolve, reject)=>{
-            db.get(pegaTattoo, id, (error,row)=>{
-                if(error)
+        const pegaTattoo = `SELECT * FROM TATUAGENS WHERE id = ?`
+        return new Promise((resolve, reject) => {
+            db.get(pegaTattoo, id, (error, row) => {
+                if (error) {
                     reject(error)
-                else
+                } else {
                     resolve(row)
+                }
             })
         })
     },
 
-    criarTatuagem : (obj) => {
+    criarTatuagem: (obj) => {
         const criaTattoo = `INSERT INTO TATUAGENS (CATEGORIA, LINK, ID_TATUADOR) VALUES (?, ?, ? )`
 
-        return new Promise((resolve, reject)=>{
-            db.run(criaTattoo,...Object.values(obj), (error,row) => {
-                if(error)
+        return new Promise((resolve, reject) => {
+            db.run(criaTattoo, ...Object.values(obj), (error, row) => {
+                if (error) {
                     reject(error)
-                else
+                } else {
                     resolve(row)
+                }
             })
         })
-    }, 
+    },
 
     ajustaTatuagem: (id, alteraImagem) => {
         const ajustaTattoo = `UPDATE TATUAGENS SET categoria = ?, link = ?, id_tatuador = ? WHERE id = ?`
-        return new Promise((resolve, reject)=>{
-           db.run(ajustaTattoo, ...Object.values(alteraImagem), id , (error,row) => {
-                if(error)
+        return new Promise((resolve, reject) => {
+            db.run(ajustaTattoo, ...Object.values(alteraImagem), id, (error, row) => {
+                if (error) {
                     reject(error)
-                else
+                } else {
                     resolve(row)
+                }
             })
         })
-    }, 
+    },
 
-    deletarTatuagem : (id) => {
+    deletarTatuagem: (id) => {
         const deletaTattoo = `DELETE FROM TATUAGENS WHERE id = ?`
         return new Promise((resolve, reject) => {
             db.run(deletaTattoo, id, (error, row) => {
-                if(error)
-                        reject(error)
-                    else
-                        resolve(row)
-                })
+                if (error) {
+                    reject(error)
+                } else {
+                    resolve(row)
+                }
+
+            })
         })
     }
 
