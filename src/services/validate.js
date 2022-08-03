ffc738d505c88fffc6d90cea1e9bd10d1ec4ed49
Rômulo Miranda <romulo.mgaudencio@gmail.com>
2022-08-03 20:24:00 -0300

    fix: ajuste no bad request do post

--- src/services/validate.js
+++ src/services/validate.js
@@ -2,11 +2,9 @@
 const validations = {
 
     validaUser : (nome, tel) => {
-        if((nome.length < 10) || (tel.length < 8)) {
-            console.log(nome, tel);
-                throw new Error("Algum campo está incompleto")
-            }
-        }
+        if(nome.length < 10) throw new Error("Seu nome deve ter ao menos 10 caracteres")
+        if(tel.length < 8) throw new Error("Telefone inválido")
+    }
 }
 
 export default validations
