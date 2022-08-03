4e60433077b813930b4a8ef91f235b3dd0b927cf
BeatrizMioranza <beatrizmioranza2508@gmail.com>
2022-08-03 19:55:51 -0300

    criei a validação da entidade Tatuagem

--- src/services/validacaoTatto.js
+++ src/services/validacaoTatto.js
@@ -1,1 +1,9 @@
-Teste
+const validaCampo = {
+    valida: (categoria, link, id_tatuador)=> {
+        if((categoria.length == 0) || (link.length == 0) || (id_tatuador.length == 0)) {
+            console.log(categoria, link, id_tatuador);
+            throw new Error("Preencha todos os campos")
+            }
+        }
+}
+export default validaCampo
