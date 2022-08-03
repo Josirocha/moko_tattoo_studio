1bfb81c82a0bfeafd89559c631d66a477162c555
Rômulo Miranda <romulo.mgaudencio@gmail.com>
2022-08-02 21:05:14 -0300

    fix: correção na middleware cors e json routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
   import express from 'express'
   import routes from './routes/index.js'
-  import cors from 'cors'
 
   const app = express()
-  
-  app.use(cors())
-  app.use(express.json())
   
   routes(app)
   
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import clientes from './clientesRoutes.js'
 import agendamentos from './agendamentosRoutes.js'
 import fornecedores from './fornecedores-routes.js'
 import tatuagens from './tatuagensRoutes.js'
 import produtos from './produtosRoutes.js'
-
-
-
 import tatuadores from './tatuadoresRoutes.js';
 
 
@@ -17,6 +15,7 @@ const routes = (app) => {
 
   app.use(
     express.json(),
+    cors(),
     clientes,
     agendamentos,
     fornecedores,
