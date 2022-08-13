fb5c7e8ac7453b3c147f4c0e0c81d09419d2788b
Erika Rodrigues <erikarodrig.contato@gmail.com>
2022-08-13 15:49:34 -0300

    ajuste em retorno das rotas

--- src/DAO/fornecedoresDAO.js
+++ src/DAO/fornecedoresDAO.js
@@ -2,73 +2,148 @@ import db from "../database/database.js"
 const fornecedoresDAO = {
     pegaFornecedores:()=> {
         return new Promise((resolve,reject)=>{
-            db.all('SELECT * FROM FORNECEDORES',(erro,linhas)=>{
-                if(erro){
-                    reject(erro)
-                    
-                }else{
-                    resolve(linhas)
+            db.all('SELECT * FROM FORNECEDORES',(error,row)=>{
+                if (error) {
+                    reject({
+                        mensagem: {
+                            mensagem: error,
+                            error: true,
+                        },
+                        status: 500,
+                    });
+                } else if (!row) {
+                    reject({
+                        mensagem: {
+                            mensagem: "Banco de dados vazio",
+                            error: true,
+                        },
+                        status: 404,
+                    });
+                } else {
+                    resolve({
+                        dados: {
+                            resultado: row,
+                        },
+                        status: 200,
+                        error: false,
+                    });
                 }
-            })
-        })
+            });
+        });
     },
+
     pegaFornecedoresId : (id) => {
         const query = `SELECT * FROM FORNECEDORES WHERE id = ?`
         return new Promise((resolve, reject) => {
             db.get(query, id, (error,row) => {
-                if(error) {
-                    reject(error)
-                } else if ((!row) || row.length <= 0) {
+                if (error) {
                     reject({
-                        "message": 'Fornecedor não encontrado',
-                        "erro": true
-                    })
-                } else {resolve(row)}
-            })
-        })
+                        mensagem: {
+                            mensagem: error,
+                            error: true,
+                        },
+                        status:500,
+                    });
+                } else if (!row) {
+                    reject({
+                        mensagem: {
+                            mensagem: "Não encontrado",
+                            error: true,
+                        },
+                        status: 404,
+                    });
+                } else {
+                    resolve({
+                        dados: {
+                            resultado: row,
+                        },
+                        status: 200,
+                        error: false,
+                    });
+                }
+            });
+        });
     },
 
-    criarFornecedor : (usuario) => {
+    criarFornecedor : (obj) => {
         const query = `INSERT INTO FORNECEDORES (NOME, TELEFONE, ENDERECO, EMAIL)
         VALUES (?, ?, ?, ?)`
 
         return new Promise((resolve, reject) => {
-            db.run(query, ...Object.values(usuario), (error,row) => {
-                if(error)
-                    reject(error)
-                else
-                    resolve(row)
-            })
-        })
+            db.run(query, ...Object.values(obj), (error) => {
+                if (error) {
+                    reject({
+                        mensagem: {
+                            mensagem: error,
+                            error: true,
+                        },
+                        status:500,
+                    });
+                } else {
+                    resolve({
+                        dados: {
+                            mensagem: "Fornecedor inserido com sucesso!"
+                        },
+                        status: 200,
+                        error: false,
+                    });
+                }
+            });
+        });
     },
 
-    atualizarFornecedor : (id, novo) => {
+    atualizarFornecedor : (id, fornecedor) => {
         const query = `UPDATE FORNECEDORES SET nome = ?, telefone = ?, endereco =?, email = ? WHERE id = ?`
 
         return new Promise((resolve, reject)=>{
-            db.run(query, ...Object.values(novo), id, (error) => {
-                    if(error)
-                        reject(error)
-                    else
-                        resolve(novo)
+            db.run(query, ...Object.values(fornecedor), id, (error) => {
+                if (error) {
+                    reject({
+                        mensagem: {
+                            mensagem: error,
+                            error: true,
+                        },
+                        status:500,
+                    });
+                } else {
+                    resolve({
+                        dados: {
+                            mensagem: "Fornecedores atualizado com sucesso!"
+                        },
+                        status: 200,
+                        error: false,
+                    });
                 }
-            )
-        })  
+            });
+        });
     },
 
     deletarFornecedor : (id) => {
         const query = `DELETE FROM FORNECEDORES WHERE id = ?`
 
         return new Promise((resolve, reject) => {
-            db.run(query, id, (error, row) => {
-                if(error)
-                        reject(error)
-                    else
-                        resolve(row)
-                })
-        })
-    }
-}
+            db.run(query, id, (error) => {
+                if (error) {
+                    reject({
+                        mensagem: {
+                            mensagem: error,
+                            error: true,
+                        },
+                        status:500,
+                    });
+                } else {
+                    resolve({
+                        dados: {
+                            mensagem: "Fornecedor deletado"
+                        },
+                        status: 200,
+                        error: false,
+                    });
+                }
+            });
+        });
+    },
+};
 
 
 
--- src/controller/fornecedores-controller.js
+++ src/controller/fornecedores-controller.js
@@ -1,39 +1,33 @@
-import dao from "../DAO/fornecedoresDAO.js"
 import fornecedoresModel from "../model/fornecedores.js"
 
 const fornecedoresController = {
     listarFornecedores: async (req, res) => {
         try {
-            const resposta = await dao.pegaFornecedores();
-            res.status(200).json(resposta);
-        } catch (e) {
-            res.status(400).json(e.message);
+            const resposta = await fornecedoresModel.pegaFornecedores();
+            res.status(resposta.status).json(resposta.dados);
+        } catch (error) {
+            res.status(error.status).json(error.mensagem);
         }
     },
 
     listarFornecedoresId: async (req, res) => {
+
         const id = req.params.id
         try {
-            const resposta = await dao.pegaFornecedoresId(id);
-            res.status(200).json(resposta);
-        } catch (e) {
-            res.status(404).json(e.message);
+            const resposta = await fornecedoresModel.pegaFornecedor(id);
+            res.status(resposta.status).json(resposta.dados);
+        } catch (error) {
+            res.status(error.status).json(error.mensagem);
         }
     },
 
     criarFornecedores: async (req, res) => {
         const body = req.body;
         try {
-            const insereFornecedor = fornecedoresModel(body.nome, body.telefone, body.endereco, body.email)
-
-            const novoFornecedor = await dao.criarFornecedor(insereFornecedor)
-            res.status(201).json({
-                "msg": `Fornecedor ${body.nome} inserido com sucesso`,
-                "Fornecedor": novoFornecedor
-            })
-
-        } catch (e) {
-            res.status(404).json(e.message);
+            const resposta = await fornecedoresModel.criaFornecedores(body)
+            res.status(resposta.status).json(resposta.dados);
+        } catch (error) {
+            res.status(error.status).json(error.mensagem);
         }
     },
 
@@ -41,35 +35,24 @@ const fornecedoresController = {
         const body = req.body
         const id = req.params.id
         try {
-            const criaFornecedor = fornecedoresModel(body.nome, body.telefone, body.endereco, body.email)
-
-            const novoFornecedor = await dao.atualizarFornecedor(id, criaFornecedor)
-            res.json({
-                "msg": `Fornecedor ${body.nome}, com id ${id}, atualizado com sucesso`,
-                "Fornecedor": novoFornecedor
-            })
-
-        } catch (e) {
-            res.json(e.message)
+            const resposta = await fornecedoresModel.atualizaFornecedor(id, body)
+            res.status(resposta.status).json(resposta.dados);
+        } catch (error) {
+            res.status(error.status).json(error.mensagem);
         }
     },
+
 
     deletarFornecedor: async (req, res) => {
         const id = req.params.id
         try {
-            await dao.deletarFornecedor(id)
-            res.json({
-                "msg": `Fornecedor${id} deletado com sucesso!`
-            })
-        } catch (e) {
-            res.json({
-                "msg": e.message
-            })
+            const resposta = await fornecedoresModel.deletaFornecedor(id)
+            res.status(resposta.status).json(resposta.dados);
+        } catch (error) {
+            res.status(error.status).json(error.mensagem);
         }
-    }
-
-}
-
+    },
+};
 
 export default fornecedoresController
 
